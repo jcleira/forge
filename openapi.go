7d23e3a1e6d365d74912accc0817da3c6149ba6d
jcleira <jmc.leira@gmail.com>
2024-07-16 17:03:07 +0200

    Draft openapi parsing #01

--- openapi.go
+++ openapi.go
@@ -8,23 +8,34 @@ import (
 )
 
 type openAPI struct {
-	OpenAPI    string              `yaml:"openapi"`
-	Info       info                `yaml:"info"`
-	Servers    []server            `yaml:"servers"`
-	Paths      map[string]pathItem `yaml:"paths"`
-	Components components          `yaml:"components"`
+	OpenAPI           string              `yaml:"openapi"`
+	Info              info                `yaml:"info"`
+	JsonSchemaDialect string              `yaml:"jsonSchemaDialect"`
+	Servers           []server            `yaml:"servers"`
+	Paths             map[string]pathItem `yaml:"paths"`
+	Components        components          `yaml:"components"`
 }
 
 type info struct {
-	Title       string  `yaml:"title"`
-	Description string  `yaml:"description"`
-	License     license `yaml:"license"`
-	Version     string  `yaml:"version"`
+	Title         string  `yaml:"title"`
+	Summary       string  `yaml:"summary"`
+	Description   string  `yaml:"description"`
+	TermOfService string  `yaml:"termOfService"`
+	Contact       contact `yaml:"contact"`
+	License       license `yaml:"license"`
+	Version       string  `yaml:"version"`
+}
+
+type contact struct {
+	Name  string `yaml:"name"`
+	URL   string `yaml:"url"`
+	Email string `yaml:"email"`
 }
 
 type license struct {
-	Name string `yaml:"name"`
-	URL  string `yaml:"url"`
+	Name       string `yaml:"name"`
+	Identifier string `yaml:"identifier"`
+	URL        string `yaml:"url"`
 }
 
 type server struct {
@@ -34,25 +45,71 @@ type server struct {
 }
 
 type variable struct {
-	Default string   `yaml:"default"`
-	Enum    []string `yaml:"enum,omitempty"`
+	Default     string   `yaml:"default"`
+	Enum        []string `yaml:"enum,omitempty"`
+	Description string   `yaml:"description"`
 }
 
 type pathItem struct {
-	Post  *operation `yaml:"post,omitempty"`
-	Get   *operation `yaml:"get,omitempty"`
-	Patch *operation `yaml:"patch,omitempty"`
+	Ref         string      `yaml:"$ref,omitempty"`
+	Summary     string      `yaml:"summary,omitempty"`
+	Description string      `yaml:"description,omitempty"`
+	Get         *operation  `yaml:"get,omitempty"`
+	Put         *operation  `yaml:"put,omitempty"`
+	Post        *operation  `yaml:"post,omitempty"`
+	Delete      *operation  `yaml:"delete,omitempty"`
+	Options     *operation  `yaml:"options,omitempty"`
+	Head        *operation  `yaml:"head,omitempty"`
+	Patch       *operation  `yaml:"patch,omitempty"`
+	Trace       *operation  `yaml:"trace,omitempty"`
+	Servers     []server    `yaml:"servers,omitempty"`
+	Parameters  []parameter `yaml:"parameters,omitempty"`
 }
 
 type operation struct {
-	Tags        []string              `yaml:"tags"`
-	Summary     string                `yaml:"summary"`
-	Description string                `yaml:"description,omitempty"`
+	Tags        []string     `yaml:"tags"`
+	Summary     string       `yaml:"summary"`
+	Description string       `yaml:"description,omitempty"`
+	ExternalDoc externalDocs `yaml:"externalDocs,omitempty"`
+	Parameters  []parameter  `yaml:"parameters,omitempty"`
+
 	OperationID string                `yaml:"operationId"`
 	Security    []map[string][]string `yaml:"security"`
 	RequestBody *requestBody          `yaml:"requestBody,omitempty"`
 	Responses   map[string]response   `yaml:"responses"`
-	Parameters  []parameter           `yaml:"parameters,omitempty"`
+}
+
+type externalDocs struct {
+	Description string `yaml:"description"`
+	URL         string `yaml:"url"`
+}
+
+type parameter struct {
+	Ref string `yaml:"$ref,omitempty"`
+
+	Name            string `yaml:"name"`
+	In              string `yaml:"in"`
+	Description     string `yaml:"description"`
+	Required        bool   `yaml:"required,omitempty"`
+	Deprecated      bool   `yaml:"deprecated"`
+	AllowEmptyValue bool   `yaml:"allowEmptyValue"`
+	Style           string `yaml:"style"`
+	Explode         bool   `yaml:"explode"`
+	allowReserved   bool   `yaml:"allowReserved"`
+	// TODO we are here
+	Schema schema `yaml:"schema"`
+}
+
+type schema struct {
+	Ref         string              `yaml:"$ref,omitempty"`
+	Type        string              `yaml:"type"`
+	Properties  map[string]property `yaml:"properties,omitempty"`
+	Items       *schema             `yaml:"items,omitempty"`
+	Required    []string            `yaml:"required,omitempty"`
+	Description string              `yaml:"description,omitempty"`
+	Enum        []string            `yaml:"enum,omitempty"`
+	Format      string              `yaml:"format,omitempty"`
+	Example     interface{}         `yaml:"example,omitempty"`
 }
 
 type requestBody struct {
@@ -62,18 +119,6 @@ type requestBody struct {
 
 type mediaType struct {
 	Schema schema `yaml:"schema"`
-}
-
-type schema struct {
-	Type        string              `yaml:"type"`
-	Properties  map[string]property `yaml:"properties,omitempty"`
-	Ref         string              `yaml:"$ref,omitempty"`
-	Items       *schema             `yaml:"items,omitempty"`
-	Required    []string            `yaml:"required,omitempty"`
-	Description string              `yaml:"description,omitempty"`
-	Enum        []string            `yaml:"enum,omitempty"`
-	Format      string              `yaml:"format,omitempty"`
-	Example     interface{}         `yaml:"example,omitempty"`
 }
 
 type property struct {
@@ -94,16 +139,6 @@ type response struct {
 	Description string               `yaml:"description"`
 	Content     map[string]mediaType `yaml:"content,omitempty"`
 	Ref         string               `yaml:"$ref,omitempty"`
-}
-
-type parameter struct {
-	Name        string `yaml:"name"`
-	In          string `yaml:"in"`
-	Description string `yaml:"description"`
-	Schema      schema `yaml:"schema"`
-	Style       string `yaml:"style"`
-	Explode     bool   `yaml:"explode"`
-	Required    bool   `yaml:"required,omitempty"`
 }
 
 type components struct {
